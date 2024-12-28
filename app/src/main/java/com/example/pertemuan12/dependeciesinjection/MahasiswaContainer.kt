8fb6b90fddd1108b3b42e186ffe2cf0733c32941
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-28 17:12:59 +0700

    Menambahkan MahasiswaContainer untuk menyediakan MahasiswaRepository dengan Retrofit

--- app/src/main/java/com/example/pertemuan12/dependeciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan12/dependeciesinjection/MahasiswaContainer.kt
@@ -12,3 +12,19 @@ interface AppContainer{
     val kontakRepository: MahasiswaRepository
 }
 
+class MahasiswaContainer : AppContainer{
+
+    private val baseUrl = "http://10.0.2.2:8000/umyTI/"
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("applucation/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy{
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy{
+        NetworkKontakRepository(mahasiswaService)
+    }
+}
