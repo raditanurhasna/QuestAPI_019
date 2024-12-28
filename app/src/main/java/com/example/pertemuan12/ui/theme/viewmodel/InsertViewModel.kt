7131b6398e7629552c8c7e10e6fee744f4229a18
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-28 23:27:28 +0700

    Menambahkan data class untuk UI state dan konversi antara InsertUiEvent dan Mahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/InsertViewModel.kt
@@ -29,3 +29,37 @@ class InsertViewModel(private val mhs: MahasiswaRepository) :ViewModel(){
     }
 }
 
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
