8d4205f4c47ba5494574843d4a0b9829c6c332e0
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-28 23:42:52 +0700

    Menambahkan HomeViewModel untuk mengelola data mahasiswa, termasuk fungsi getMhs dan deleteMhs

--- app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/HomeViewModel.kt
@@ -18,5 +18,37 @@ sealed class HomeUiState {
     object Loading:HomeUiState()
 }
 
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+    private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e:IOException){
+                HomeUiState.Error
+            }catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+}
 
 
