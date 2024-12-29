8fa685ecdc34179a69c0d6c5fb3a64e419d58875
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-29 13:21:30 +0700

    Menambah fungsi CostumeTopAppBar dengan navigasi dan refresh

--- app/src/main/java/com/example/pertemuan12/ui/theme/customewidget/TopAppBar.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/customewidget/TopAppBar.kt
@@ -1,2 +1,28 @@
 package com.example.pertemuan12.ui.theme.customewidget
 
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {},
+) {
+
+}
