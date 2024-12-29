20db8fb9d20475f1ce119d7660d72145af32ed5b
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-29 22:45:10 +0700

    Menambahkan PenyediaViewModel

---
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan12.ui.theme.viewmodel
+
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan12.MahasiswaApplications
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+
+    }
+    fun CreationExtras.aplikasiKontak():MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
+}
