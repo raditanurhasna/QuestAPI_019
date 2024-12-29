889f90981543718f5e72ae5aa805978ea54cdf8f
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-29 14:47:59 +0700

    Menambah layar EntryMhs

--- app/src/main/java/com/example/pertemuan12/ui/theme/viewmahasiswa/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmahasiswa/InsertView.kt
@@ -33,3 +33,41 @@ object DestinasiEntry:DestinasiNavigasi{
     override val titleRes="Entry Mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack:()->Unit,
+    modifier: Modifier=Modifier,
+    viewModel: InsertViewModel= viewModel(factory=PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
+}
+
