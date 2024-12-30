06cfeefe1ded10d25d1b247a8997b16381ae38d6
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-30 12:16:27 +0700

    Manambahkan destinasi navigasi untuk detail mahasiswa

---
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmahasiswa/DetailMhsView.kt
@@ -0,0 +1,41 @@
+package com.example.pertemuan12.ui.theme.viewmahasiswa
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.ui.theme.navigation.DestinasiNavigasi
+import com.example.pertemuan12.ui.theme.viewmodel.DetailMhsViewModel
+import com.example.pertemuan12.ui.theme.viewmodel.PenyediaViewModel
+
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail/{nim}"
+    override val titleRes = "Detail Mahasiswa"
+    const val NIM = "nim"
+    val routeWithArgs = "$route/{$NIM}"
+}
+
