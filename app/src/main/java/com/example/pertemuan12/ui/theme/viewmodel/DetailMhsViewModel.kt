4472db5ff7972e4ed6732572e04b4860eea9754e
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-30 11:59:05 +0700

    Manambah fungsi fungsi untuk ambil detail mahasiswa

---
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.pertemuan12.ui.theme.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class DetailMhsViewModel(private val mhsRepository: MahasiswaRepository) : ViewModel() {
+
+    var uiState by mutableStateOf(DetailUiState())
+        private set
+
+    fun fetchDetailMahasiswa(nim: String) {
+        viewModelScope.launch {
+            uiState = DetailUiState(isLoading = true)
+            try {
+                val mahasiswa = mhsRepository.getMahasiswabyNim(nim)
+                uiState = DetailUiState(detailUiEvent = mahasiswa.toDetailUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailUiState(isError = true, errorMessage = "Failed to fetch details: ${e.message}")
+            }
+        }
+    }
+
+}
+
